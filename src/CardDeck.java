0849a8f7b8cc64c7098faf1a80aa8ebf777f137f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-14 19:51:26 +0300

    initialize instance variables of the class

---
+++ src/CardDeck.java
@@ -0,0 +1,6 @@
+public class CardDeck {
+    private static final ArrayList deckCards = new ArrayList(52);
+    private String[] cardRanks = {"two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"};
+    private Suit[] cardSuits = {new Suit("diamonds", "red"), new Suit("cubs", "black"), new Suit("hearts", "red"), new Suit("hearts", "red")};
+
+}
