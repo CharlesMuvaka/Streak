6f144743f1e91b76f1f262a49afa3661e82d01c4
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-14 19:13:47 +0300

    create a method to shuffle all the cards present in the arraylist

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class ArrayList {
     private Card[] cards;
     private int capacity, size;
@@ -26,5 +28,14 @@ public class ArrayList {
         return this.size;
     }
 
-
+    //create a method to shuffle the arrayList
+    public void shuffleCards(){
+        Random randomNumber = new Random();
+        for (int i = 0; i < cards.length; i++){
+            int secondCard = randomNumber.nextInt(cards.length); //generate a random number based from the length of the cards
+            Card newCard = cards[i];
+            cards[i] = cards[secondCard];
+            cards[secondCard] = newCard;
+        }
+    }
 }
