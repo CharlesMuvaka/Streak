3a3db30295de6774d5b4f1b9c2f26a3376dae012
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-14 19:15:44 +0300

    create a method to get a card from the arrayList based on its index

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -32,10 +32,15 @@ public class ArrayList {
     public void shuffleCards(){
         Random randomNumber = new Random();
         for (int i = 0; i < cards.length; i++){
-            int secondCard = randomNumber.nextInt(cards.length); //generate a random number based from the length of the cards
+            int secondCard = randomNumber.nextInt(cards.length); //generates a random number based from the length of the cards
             Card newCard = cards[i];
             cards[i] = cards[secondCard];
             cards[secondCard] = newCard;
         }
+    }
+
+    //create a method that gets a card from the arraylist based on its index
+    public Card getCard(int index){
+        return this.cards[index];
     }
 }
