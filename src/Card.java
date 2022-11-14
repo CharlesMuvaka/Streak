27f8d48969cfc511c4953bce41a3294aa0dbbab9
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-14 19:19:00 +0300

    create an argument based constructor to be used in the initialization of the Card class

--- src/Card.java
+++ src/Card.java
@@ -2,5 +2,10 @@ public class Card {
     private String suit, rank, suitColor;
     private int cardValue;
 
-
+    public Card(String suit, String rank, String suitColor, int cardValue) {
+        this.suit = suit;
+        this.rank = rank;
+        this.suitColor = suitColor;
+        this.cardValue = cardValue;
+    }
 }
