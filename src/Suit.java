31c7c722f35a736678896a810778addf325270a1
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-14 19:25:46 +0300

    generate getters and setters of the class

--- src/Suit.java
+++ src/Suit.java
@@ -7,4 +7,20 @@ public class Suit {
         this.name = name;
         this.color = color;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
