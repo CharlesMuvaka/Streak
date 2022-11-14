af5a7b3d279bf48679c3aa99027baacabc20e745
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-14 19:00:11 +0300

    create a constructor to initalise an instance of the arrayList

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -2,5 +2,9 @@ public class ArrayList {
     private Card[] cards;
     private int capacity, size;
 
-
+    public ArrayList(int capacity) {
+        this.cards = new Card[capacity];
+        this.capacity = capacity;
+        this.size = 0;
+    }
 }
