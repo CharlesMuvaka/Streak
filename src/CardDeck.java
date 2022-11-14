94f749968b5ff6b73ad1a2c75e77f931884230be
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-14 19:52:10 +0300

    create a method to create the default values of the deck cards

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -3,4 +3,37 @@ public class CardDeck {
     private String[] cardRanks = {"two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"};
     private Suit[] cardSuits = {new Suit("diamonds", "red"), new Suit("cubs", "black"), new Suit("hearts", "red"), new Suit("hearts", "red")};
 
+    private static int getDefaultCardValues(String rank){
+        switch(rank){
+            case "two":
+                return 2;
+            case "three":
+                return 3;
+            case "four":
+                return 4;
+            case "five":
+                return 5;
+            case "six":
+                return 6;
+            case "seven":
+                return 7;
+            case "eight":
+                return 8;
+            case "nine":
+                return 9;
+            case "ten":
+                return 10;
+            case "jack":
+                return 11;
+            case "queen":
+                return 12;
+            case "king":
+                return 13;
+            case "ace":
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
 }
