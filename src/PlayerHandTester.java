aa4dd24ab138f6f3cea51e3b6911d9dcfd59c340
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-15 12:14:07 +0300

    create a test to check shuffling of the cardDeck

--- src/PlayerHandTester.java
+++ src/PlayerHandTester.java
@@ -1,8 +1,15 @@
 public class PlayerHandTester {
+    static CardDeck deck = new CardDeck();
 
     //method to test initialization of the card deck of 52 cards
     public static CardDeck testCardDeck(){
-        return new CardDeck();
+        return deck;
+    }
+
+    //method to check shuffle method of the customArrayList
+    public static CardDeck shuffleCardDeck(){
+        deck.getDeckCards().shuffleCards();
+        return deck;
     }
 
 
