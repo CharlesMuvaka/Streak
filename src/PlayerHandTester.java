1639973ca2847e48a834aefa5ead4406fc3e1a1a
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-15 11:56:27 +0300

    create a test to check the initialization of the cardDeck

---
+++ src/PlayerHandTester.java
@@ -0,0 +1,11 @@
+public class PlayerHandTester {
+
+    //method to test initialization of the card deck of 52 cards
+    public static CardDeck testCardDeck(){
+        return new CardDeck();
+    }
+
+
+
+
+}
